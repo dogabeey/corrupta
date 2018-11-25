b242731f65df37a90ad51584604fb5992768f95b
dogabeey@gmail.com <dogabeey@gmail.com>
2018-11-25 13:35:05 +0300

    parliament ui

--- Assets/Scripts/Party.cs
+++ Assets/Scripts/Party.cs
@@ -5,21 +5,21 @@ using UnityEngine.UI;
 
 public class Party : MonoBehaviour
 {
-    static List<Party> parties = new List<Party>();
+    public static List<Party> parties = new List<Party>();
 
-    public string name;
+    public string partyName;
     public string logo;
 
     public Person founder;
     public Person chairPerson;
-    public Person[] viceChairPerson;
-    public Person[] deputyList;
-    public Person[] informalMembers;
+    public Person viceChairPerson;
+    public List<Person> deputyList;
+    public List<Person> informalMembers;
     public Ideology ideology;
 
     public Party(string name, string logo, Person founder,Ideology ideology)
     {
-        this.name = name;
+        this.partyName = name;
         this.logo = logo;
         this.founder = founder;
         this.ideology = ideology;
---
+++ Assets/Scripts/UI/ParliamentUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParliamentUI : MonoBehaviour
+{
+    public GameObject chairPrefab;
+
+    private void Start()
+    {
+        //nameText = GetComponentsInChildren<Text>()[0];
+        //partyText = GetComponentsInChildren<Text>()[1];
+
+        //nameText.text = deputy.firstName + deputy.lastName;
+        //partyText.text = Party.parties.Find(pt => pt.deputyList.Find(pr => pr == deputy) == deputy).name; // wtf
+    }
+}
