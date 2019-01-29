75fa05aa72c72681dadb5b88dc4d93fceadb0247
dogabeey@gmail.com <dogabeey@gmail.com>
2019-01-29 03:52:02 +0300

    menu control implement

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuControl : MonoBehaviour
 {
+    public GameObject contentPanel;
 
     public void LoadScene(string scene)
     {
         LoadScene(scene);
+    }
+
+    public void DeactivateContent()
+    {
+        for (int i = 0; i < contentPanel.transform.childCount; i++)
+        {
+            contentPanel.transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 }
