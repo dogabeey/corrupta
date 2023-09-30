841ef6672e88b63940fa1c4daf8e554e97686c41
dogacankilic <doga.kilic@gametator.com>
2023-10-01 00:08:09 +0300

    burdayım be burdayım

--- Assets/Scripts/Map/MapDrawer.cs
+++ Assets/Scripts/Map/MapDrawer.cs
@@ -6,22 +6,18 @@ using UnityEngine.Scripting;
 
 public class MapDrawer : MonoBehaviour
 {
-    public string textureName = "_MainTex";
-    [NonSerialized] public Color selectedColor;
+    public MeshRenderer meshRenderer;
+    public string provinceSelectString = "_ProvinceSelect";
+    public string provinceColorString = "_ProvinceColor";
+    [HideInInspector] public Color selectedColor;
 
     Texture2D tex, originalTex;
-    Material material;
     float mapHeight, mapWidth;
 
     void Start()
     {
-        selectedColor = Color.black;
-        material = GetComponent<Renderer>().material;
-        originalTex = (Texture2D)material.GetTexture(textureName);
         mapHeight = originalTex.height;
         mapWidth = originalTex.width;
-        tex = new Texture2D(originalTex.width, originalTex.height, originalTex.format, false);
-        tex.SetPixels(originalTex.GetPixels());
     }
 
     void Update()
@@ -42,26 +38,9 @@ public class MapDrawer : MonoBehaviour
 
     void SelectColor(Color color)
     {
+        meshRenderer.material.SetInt(provinceSelectString, color == Color.black ? 0 : 1);
+        meshRenderer.material.SetColor(provinceColorString, color);
+
         selectedColor = color;
-        Destroy(GameObject.FindGameObjectWithTag("province"));
-        GameObject selectionMask = Instantiate(gameObject);
-        selectionMask.layer = 9; // mask map layer
-        selectionMask.GetComponent<MeshCollider>().enabled = false; // Prevent rays collide masks
-        selectionMask.GetComponent<Animator>().enabled = true;
-        selectionMask.tag = "province";
-        Texture2D tex2 = new Texture2D(originalTex.width, originalTex.height, originalTex.format, false);
-        tex2.SetPixels(originalTex.GetPixels());
-        selectionMask.GetComponent<Renderer>().material.mainTexture = tex2;
-        selectionMask.GetComponent<Renderer>().material.SetFloat("_BumpScale", 0);
-        Color[] pixels = tex2.GetPixels();
-        for (int i = 0; i < pixels.Length; i++)
-        {
-            if(pixels[i] != color)
-            {
-                pixels[i].a = 0;
-            }
-        }
-        tex2.SetPixels(pixels);
-        tex2.Apply();
     }
 }
