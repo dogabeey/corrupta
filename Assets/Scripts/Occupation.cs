7a27d7015655aee19473427b0555c1c01ad61b9d
Doğa <doga@imaginaryworlds.inc>
2025-12-10 14:39:14 +0300

    add several new occupations

--- Assets/Scripts/Occupation.cs
+++ Assets/Scripts/Occupation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 
 [CreateAssetMenu(fileName = "New Occupation", menuName = "Corrupta/Social Classes/New Occupation...")]
 public class Occupation : ListedScriptableObject<Occupation>
@@ -15,4 +16,37 @@ public class Occupation : ListedScriptableObject<Occupation>
     public Vector2 educationRange;
     [MinMaxSlider(0, 100)]
     public Vector2 partizanshipRange;
+
+    private void OnValidate()
+    {
+        if(id == 0)
+        {
+            var allOccupations = GetInstances();
+            int maxId = 0;
+            foreach (var occupation in allOccupations)
+            {
+                if (occupation.id > maxId)
+                {
+                    maxId = occupation.id;
+                }
+            }
+            id = maxId + 1;
+        }
+        if(className.IsNullOrWhitespace())
+        {
+            className = name;
+        }
+        if(wealthRange.x == wealthRange.y)
+        {
+            wealthRange.y = Mathf.Min(wealthRange.x + 10, 100);
+        }
+        if(educationRange.x == educationRange.y)
+        {
+            educationRange.y = Mathf.Min(educationRange.x + 10, 100);
+        }
+        if(partizanshipRange.x == partizanshipRange.y)
+        {
+            partizanshipRange.y = Mathf.Min(partizanshipRange.x + 10, 100);
+        }
+    }
 }
