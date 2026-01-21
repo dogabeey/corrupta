c08d27cc8231a8cd981c91364d26a5c8db2c1b68
dogabeey <dogabeey@gmail.com>
2026-01-21 16:36:13 +0300

    example advisor types

--- Assets/Scripts/Advisor.cs
+++ Assets/Scripts/Advisor.cs
@@ -11,3 +11,44 @@ public abstract class AdvisorType
     public abstract Sprite AdvisorIcon { get; }
     public abstract float CostMultiplier { get; }
 }
+
+public class PRDirector : AdvisorType
+{
+    public override string AdvisorName => "Public Relations Director";
+    private List<AdvisorAbility> abilities = new List<AdvisorAbility>()
+    {
+    };
+    public override List<AdvisorAbility> Abilities => abilities;
+    public override Sprite AdvisorIcon => null;
+    public override float CostMultiplier => 1f;
+}
+public class ChiefOfStaff : AdvisorType
+{
+    public override string AdvisorName => "Chief of Staff";
+    private List<AdvisorAbility> abilities = new List<AdvisorAbility>()
+    {
+    };
+    public override List<AdvisorAbility> Abilities => abilities;
+    public override Sprite AdvisorIcon => null;
+    public override float CostMultiplier => 1f;
+}
+public class FinanceChairman : AdvisorType
+{
+    public override string AdvisorName => "Finance Chairman";
+    private List<AdvisorAbility> abilities = new List<AdvisorAbility>()
+    {
+    };
+    public override List<AdvisorAbility> Abilities => abilities;
+    public override Sprite AdvisorIcon => null;
+    public override float CostMultiplier => 1f;
+}
+public class OppositionResearcher : AdvisorType
+{
+    public override string AdvisorName => "Opposition Researcher";
+    private List<AdvisorAbility> abilities = new List<AdvisorAbility>()
+    {
+    };
+    public override List<AdvisorAbility> Abilities => abilities;
+    public override Sprite AdvisorIcon => null;
+    public override float CostMultiplier => 1f;
+}
