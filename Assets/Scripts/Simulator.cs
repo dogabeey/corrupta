8df4722d83e6a49040469a6272dcd8907f210567
Doğa <dogabeey@gmail.com>
2026-02-18 18:33:39 +0300

    turn simulator class (WIP)

--- Assets/Scripts/Simulator.cs
+++ Assets/Scripts/Simulator.cs
@@ -5,15 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Simulator: MonoBehaviour
 {
-    public int turn = 0;
-    [Range(1,4)] public int simSpeed = 1;
+    public int turn = 0; // Each turn is a week. 52 turns is a year.
 
-    public int Year { get { return (int)(turn / 12f); } }
-    public int Month { get { return (turn % 12) + 1; } }
-
-    public Text yearText;
-    public Text monthText;
-    public GameObject gameCanvas;
+    public int Year { get { return turn / 52 + 1; } }
+    public int Month { get { return (turn % 52) / 4 + 1; } }
+    public string MonthText { get { return MonthIntToText(); } }
 
     public void Start()
     {
@@ -24,21 +20,33 @@ public class Simulator: MonoBehaviour
         }
     }
     
-    public void SetSpeed(Slider value)
+    private string MonthIntToText()
     {
-
-        simSpeed = (int)value.value;
+        return Month switch
+        {
+            1 => "January",
+            2 => "February",
+            3 => "March",
+            4 => "April",
+            5 => "May",
+            6 => "June",
+            7 => "July",
+            8 => "August",
+            9 => "September",
+            10 => "October",
+            11 => "November",
+            12 => "December",
+            _ => "Unknown",
+        };
     }
 
     public void NextTurn(bool invokeEvents = true)
     {
-        // event affects right before new day.
+        // event affects right before new week.
+        EventManager.TriggerEvent(GameConstants.GameEvents.TURN_PASSED);
 
-        // passing of day
+        // passing of a week
         turn++;
-        yearText.text = (Year + DateTime.Now.Year).ToString();
-        monthText.text = Month.ToString();
-
         Country.Instance.UpdateAll();
     }
 
